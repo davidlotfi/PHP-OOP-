75a53a32dbcbdcb821cdbba7ddb530134f5ec7ce
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-02-03 21:56:11 +0100

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -2,26 +2,18 @@
   require_once("../includes/config.php");
   require_once("../includes/database.php");
   require_once("../includes/user.php");
-  
- 
+   
   // User:: because the methods is static :)
-
-  $found_user = User::find_by_id(1);
-  $user = new User();
-  $user->id=$found_user['id'];
-  $user->username=$found_user['username'];
-  $user->password=$found_user['password'];
-  $user->first_name=$found_user['first_name'];
-  $user->last_name=$found_user['last_name'];
-
+  $user = User::find_by_id(1);
   echo $user->full_name();
-  
+
   echo "<hr />";
 
-  // $user_set = User::find_all();
-  // while ($user = $database->fetch_array($user_set)) {
-  //   echo "User : ".$user['username']."<br />";
-  //   echo "Name : ".$user['last_name']." ".$user['first_name']."<br />";
-  // }
+  $users = User::find_all();
+ foreach($users as $user) {
+  echo "User: ". $user->username ."<br />";
+  echo "Name: ". $user->full_name() ."<br /><br />";
+  }
 
+ 
 ?>
