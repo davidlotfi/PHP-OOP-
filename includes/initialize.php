946c43f9daf736abe8e343c7c8a8a67544250053
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-02-05 22:31:28 +0100

    Add files via upload

---
+++ includes/initialize.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("config.php");
+require_once("functions.php");
+require_once("session.php");
+require_once("database.php");
+require_once("user.php");
+
+
+?>
--- includes/user.php
+++ includes/user.php
@@ -28,7 +28,19 @@ class User {
     }
     return $object_array;
   }
+  public static function authenticate($username="", $password="") {
+    global $database;
+    $username = $database->escape_value($username);
+    $password = $database->escape_value($password);
 
+    $sql  = "SELECT * FROM users ";
+    $sql .= "WHERE username = '{$username}' ";
+    $sql .= "AND password = '{$password}' ";
+    $sql .= "LIMIT 1";
+    $result_array = self::find_by_sql($sql);
+		return !empty($result_array) ? array_shift($result_array) : false;
+  }
+  
   public function full_name() {
     if(isset($this->first_name) && isset($this->last_name)) {
       return $this->first_name . " " . $this->last_name;
