427dc98a4849b5968cb43de381648e7395e12e58
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-02-05 22:30:57 +0100

    Add files via upload

--- includes/functions.php
+++ includes/functions.php
@@ -21,5 +21,15 @@
             return "";
         }
     }
+    function __autoload($class_name){
+        $class_name = strtolower($class_name);
+        $path="../includes/{$class_name}.php";
+        if (file_exists($path)) {
+             require_once($path);
+        }else{
+          die("The file {$class_name}.php could not be found.");
+        }
+
+    }
 
 ?>
