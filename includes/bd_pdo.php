c37d47e4c47ea2278e2d989e321c78cd1e98b57e
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-02-23 22:32:35 +0100

    Add files via upload

--- includes/bd_pdo.php
+++ includes/bd_pdo.php
@@ -1,11 +1,17 @@
 <?php
-  // with PDO extention database 
-try {
+  // with PDO extention database
+  try {
 
-  $bdd = new PDO('mysql:host=localhost;dbname=photo_gallery','root','');
+    $bdd = new PDO('mysql:host=localhost;dbname=photo_gallery','root','');
 
-   } catch (Exception $e) {
-       die('Erreur :'.$e->getMessage());
- }
+     } catch (Exception $e) {
+       
+         die('Erreur :'.$e->getMessage());
+   }
+
+    function find_all_photo(){
+      return $bdd->query('SELECT * FROM photographs');
+    }
+
 
 ?>
