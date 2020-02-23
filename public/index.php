95edaa76085b2aca87293be309d4b066f2ff7502
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-02-23 19:15:41 +0100

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,42 +1,42 @@
-<<<<<<< HEAD
 <?php
   require_once("../includes/functions.php");
   require_once("../includes/config.php");
   require_once("../includes/database.php");
   require_once("../includes/user.php");
+    // User:: because the methods is static :)
+
    
-  // User:: because the methods is static :)
-  $user = User::find_by_id(1);
-  echo $user->full_name();
-
-  echo "<hr />";
-
-  $users = User::find_all();
- foreach($users as $user) {
-  echo "User: ". $user->username ."<br />";
-  echo "Name: ". $user->full_name() ."<br /><br />";
-  }
-
- 
-=======
-<?php
-  require_once("../includes/functions.php");
-  require_once("../includes/config.php");
-  require_once("../includes/database.php");
-  require_once("../includes/user.php");
-   
-  // User:: because the methods is static :)
-  $user = User::find_by_id(1);
-  echo $user->full_name();
-
-  echo "<hr />";
-
-  $users = User::find_all();
- foreach($users as $user) {
-  echo "User: ". $user->username ."<br />";
-  echo "Name: ". $user->full_name() ."<br /><br />";
-  }
-
- 
->>>>>>> 39c87dba027025562d25ba6ceb878195fdef5ebf
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="stylesheets/main.css">
+  <title>Photo Gallery</title>
+</head>
+<body>
+  <div id="header"><h1>Photo Gallery</h1></div>
+  <div id="main">
+
+     <?php
+        
+        $user = User::find_by_id(1);
+        echo $user->full_name();
+        echo "<hr />";
+      
+        $users = User::find_all();
+       foreach($users as $user) {
+        echo "User: ". $user->username ."<br />";
+        echo "Name: ". $user->full_name() ."<br /><br />";
+        }
+       
+     ?>
+
+      
+		
+	</div>
+  <div id="footer">Copyright <?php echo date("Y", time());?>, Boukernine Abdellatif</div>
+</body>
+</html>
