75e9eeb0a0e43846ee4b91f92cd8874d23cba5aa
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-02-24 15:05:55 +0100

    Add files via upload

--- public/admin/list_photo.php
+++ public/admin/list_photo.php
@@ -1,7 +1,7 @@
 <?php
-require_once("../../includes/functions.php");
-require_once("../../includes/session.php");
-if (!$session->is_logged_in()) {redirect_to("login.php");}
+  require_once("../../includes/bd_pdo.php");
+  require_once("../../includes/session.php");
+  if (!$session->is_logged_in()) {redirect_to("login.php");}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -12,9 +12,29 @@ if (!$session->is_logged_in()) {redirect_to("login.php");}
   </head>
   <body>
     <div id="header"><h1>Photo Gallery</h1></div>
-
     <div id="main">
-      <a href="index.php"><< Back</a>
+      <h3>Photographs</h3>
+
+      <a href="index.php"><< Back</a><br><br>
+      <?php $result = find_all_photo(); ?>
+      <table class="bordered">
+        <tr>
+          <th>Image</th>
+          <th>Filename</th>
+          <th>Caption</th>
+          <th>Size</th>
+          <th>Type</th>
+        </tr>
+        <?php while ($donnes = $result->fetch()) { ?>
+          <tr>
+            <td><img src="../images/<?php echo $donnes['filename']; ?>" width="100" /></td>
+            <td><?php echo $donnes['filename'];?></td>
+            <td><?php echo $donnes['caption']; ?></td>
+            <td><?php echo $donnes['size']; ?></td>
+            <td><?php echo $donnes['type'] ;?></td>
+          </tr>
+      <?php }; ?>
+      </table>
 
 
     </div>
