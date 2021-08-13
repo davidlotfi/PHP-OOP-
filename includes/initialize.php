65eeb73136841dfa6deee7d9933815767915e32d
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2021-08-14 01:14:49 +0200

    Update initialize.php

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,10 +1,8 @@
 <?php
-
 require_once("config.php");
 require_once("functions.php");
 require_once("session.php");
 require_once("database.php");
 require_once("bd_pdo.php");
 require_once("user.php");
-
 ?>
